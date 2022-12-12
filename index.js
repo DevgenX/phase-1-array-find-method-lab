24449dba80f984880f2c2ded084239f7f48309c8
devgenx <sebastian110420@gmail.com>
2022-12-12 14:27:30 -0500

    commit tree a18d6d3718d9a39cae655b948a5fede10df6ce97

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+
+const superbowlWin = (records) => {
+  let Winner = records.find((record) => record.result === "W");
+
+  if (Winner) {
+    return Winner.year;
+  }
+};
